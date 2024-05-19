d8ecf2dc2a1e558d69b44f3fc4382196f94eac5a
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-05-19 08:20:28 -0300

    :tada: Inicializando o projeto

---
+++ java_cources/src/main/java/br/com/daianefarias/java_cources/JavaCourcesApplication.java
@@ -0,0 +1,13 @@
+package br.com.daianefarias.java_cources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourcesApplication.class, args);
+	}
+
+}
---
+++ java_cources/src/test/java/br/com/daianefarias/java_cources/JavaCourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daianefarias.java_cources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
